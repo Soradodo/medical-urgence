95b39b47f24ea52f413fad7b4f7c9080cc9d132c
Your Name <dorianelf7@gmail.com>
2026-04-04 21:38:06 -0400

    fix: register clean + alias @

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require('path');
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  }
+};
 module.exports = nextConfig;
